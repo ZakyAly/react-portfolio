1b12e99a4a27421793428f99f0dfc253ed622d85
zaky aly <zaky3ly@gmail.com>
2024-07-31 00:42:52 +0200

    building Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,96 @@
 import React from "react";
-// import styles from "./HeaderStyle.module.css";
+import styles from "./HeaderStyle.module.css";
+
+import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
+import { useTheme } from "../../common/ThemeContext";
+
 
 const Header = () => {
-  return <div>Header</div>;
+  const { theme, toggleTheme } = useTheme();
+  const themeIcon = theme === "light" ? sun : moon;
+
+  return (
+    <>
+      <nav id="desktop-nav">
+        <div className={styles.logo}>Zaky Aly</div>
+        <div>
+          <ul className={styles.navLinks}>
+            <li>
+              <a href="#about">About Me</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#skills">Skills</a>
+            </li>
+            <li>
+              <a href="#experience">Experience</a>
+            </li>
+            <li>
+              <a href="#courses">Courses</a>
+            </li>
+            <li>
+              <a href="#contact">Contact Me</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <img
+          src={themeIcon}
+          alt="Theme mode icon"
+          className={styles.colorMode}
+          onClick={toggleTheme}
+        />
+        </div>
+      </nav>
+      <nav id="nav-menu">
+        <div className="logo">Zaky Aly</div>
+        <div className="dropdown-menu">
+          <div class="menu-icon" onclick="toggleMenu()">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <div>
+          <ul className={styles.navLinks}>
+            <li>
+              <a href="#about" onclick="toggleMenu()">
+                About Me
+              </a>
+            </li>
+            <li>
+              <a href="#projects" onclick="toggleMenu()">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#skills" onclick="toggleMenu()">
+                Skills
+              </a>
+            </li>
+            <li>
+              <a href="#experience" onclick="toggleMenu()">
+                Experience
+              </a>
+            </li>
+            <li>
+              <a href="#courses" onclick="toggleMenu()">
+                Courses
+              </a>
+            </li>
+            <li>
+              <a href="#contact" onclick="toggleMenu()">
+                Contact Me
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Header;
