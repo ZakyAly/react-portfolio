36d85ac74f1c956737255881e7af7e142db1827c
zaky aly <zaky3ly@gmail.com>
2024-08-02 23:12:57 +0200

    restyle the Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,60 +1,59 @@
 import React from "react";
-import styles from './HeroStyle.module.css'
-import heroImg1 from '../../assets/hero-img1.png'
-import sun from '../../assets/sun.svg'
-import moon from '../../assets/moon.svg'
-import linkedinLight from '../../assets/linkedin-light.svg'
-import linkedinDark from '../../assets/linkedin-dark.svg'
-import githubLight from '../../assets/github-light.svg'
-import githubDark from '../../assets/github-dark.svg'
-import CV  from '../../assets/zaky-aly-cv.pdf'
+import styles from "./HeroStyle.module.css";
+import heroImg from "../../assets/hero-img.png";
+// import sun from "../../assets/sun.svg";
+// import moon from "../../assets/moon.svg";
+import linkedinLight from "../../assets/linkedin-light.svg";
+import linkedinDark from "../../assets/linkedin-dark.svg";
+import githubLight from "../../assets/github-light.svg";
+import githubDark from "../../assets/github-dark.svg";
+import CV from "../../assets/zaky-aly-cv.pdf";
 import { useTheme } from "../../common/ThemeContext";
 
 const Hero = () => {
-  const {theme, toggleTheme} = useTheme();
+  const { theme, toggleTheme } = useTheme();
 
-  const themeIcon = theme === 'light' ? sun : moon;
-  const githubIcon = theme === 'light' ? githubLight: githubDark;
-  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+  // const themeIcon = theme === "light" ? sun : moon;
+  const githubIcon = theme === "light" ? githubLight : githubDark;
+  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
 
-  return(
+  return (
     <section id="hero" className={styles.container}>
-      <div className={styles.colorModeContainer}>
+      {/* <div className={styles.colorModeContainer}> */}
+      <div className={styles.hero}>
         <img
-          className={styles.heroImg}  
-          src={heroImg1}
+          className={styles.heroImg}
+          src={heroImg}
           alt="Profile picture of Zaky Aly"
         />
-        <img
+        {/* <img
           src={themeIcon}
           alt="Theme mode icon"
           className={styles.colorMode}
-          onClick={toggleTheme}/>
+          onClick={toggleTheme}
+        /> */}
       </div>
       <div className={styles.info}>
-        <h1>
-          Zaky
-          <br />
-          Aly
-        </h1>
+        <p className={styles.description}>Hello, I'm</p>
+        <h1>Zaky Aly</h1>
         <h2>Frontend Developer</h2>
         <span>
-        <a href="https://github.com/ZakyAly" target="_blank">
+          <a href="https://github.com/ZakyAly" target="_blank">
             <img src={githubIcon} alt="" />
           </a>
           <a href="https://www.linkedin.com/in/zakyaly/" target="_blank">
             <img src={linkedinIcon} alt="linkedin" />
           </a>
         </span>
-          <p>With a passion for building and developing modern web apps.</p>
-          <a href={CV} download>
-            <button className="hover" >
-              Resume
-            </button>
-          </a>
+        {/* <p className={styles.description}>
+          With a passion for building and developing modern web apps.
+        </p> */}
+        <a href={CV} download>
+          <button className="hover">Resume</button>
+        </a>
       </div>
     </section>
-  )
+  );
 };
 
 export default Hero;
